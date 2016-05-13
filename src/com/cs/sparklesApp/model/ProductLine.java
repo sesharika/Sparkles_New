61724530480c2745f32dab1eba66049df8908ec9
Sesharika <Sesharika@Celery>
2016-05-13 15:09:37 -0700

    New Sparkles App

---
+++ src/com/cs/sparklesApp/model/ProductLine.java
@@ -0,0 +1,49 @@
+package com.cs.sparklesApp.model;
+
+import java.sql.Blob;
+
+public class ProductLine {
+	private String productLine;
+	private String textDescription;
+	private String htmlDescription;
+	private Blob image;
+	
+	public ProductLine(String productLine) {
+		super();
+		this.productLine = productLine;
+	}
+	public ProductLine(String productLine, String textDescription, String htmlDescription, Blob image) {
+		super();
+		this.productLine = productLine;
+		this.textDescription = textDescription;
+		this.htmlDescription = htmlDescription;
+		this.image = image;
+	}
+	public String getProductLine() {
+		return productLine;
+	}
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+	public String getTextDescription() {
+		return textDescription;
+	}
+	public void setTextDescription(String textDescription) {
+		this.textDescription = textDescription;
+	}
+	public String getHtmlDescription() {
+		return htmlDescription;
+	}
+	public void setHtmlDescription(String htmlDescription) {
+		this.htmlDescription = htmlDescription;
+	}
+	public Blob getImage() {
+		return image;
+	}
+	public void setImage(Blob image) {
+		this.image = image;
+	}
+	
+	
+	
+}
